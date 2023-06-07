1ecfec19f945a75709a3e61f8b7cffc924a70184
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-06-07 23:15:11 +0530

    intial commit

--- pascal_triangle.java
+++ pascal_triangle.java
@@ -13,8 +13,8 @@ public class pascal_triangle {
     public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n= sc.nextInt();
-		for(int i=1; i<=n; i++ ){
-			for(int j=1; j<=i; j++){
+		for(int i=0; i<n; i++ ){
+			for(int j=0; j<=i; j++){
 				System.out.print(p(i,j)+"	");
 				}
 			System.out.println();
