83bc78e786e929c335aa1ca2ab91d218f1db4251
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-06-07 22:25:36 +0530

    intial commit

---
+++ pascal_triangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class pascal_triangle {
+	static int f(int n){
+		int temp=1;
+		for(int x=1; x<=n;x++)
+			temp*=x;
+		return(temp);
+	}
+	static int p(int i, int j){
+		int t = f(i)/(f(i-j)*f(j));
+		return(t);
+	}
+    public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		for(int i=1; i<=n; i++ ){
+			for(int j=1; j<=i; j++){
+				System.out.print(p(i,j)+"	");
+				}
+			System.out.println();
+			}
+		}
+    }
