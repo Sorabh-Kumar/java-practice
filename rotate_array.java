3d81859c217889feabef429254207285ac8a3098
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-06-27 00:07:47 +0530

    NA correction

--- rotate_array.java
+++ rotate_array.java
@@ -5,8 +5,8 @@ public static void rot(int[] arr, int r) {
     r = r % arr.length;
     for(int i = 0 ; i<r; i++){
         int l = arr[arr.length-1];
-        for(int j = arr.length; j>0; j--){
-            arr[j]= arr[j-1];
+        for(int j = arr.length -1; j>0; j--){
+            arr[ j ]= arr[j - 1];
         }
         arr[0] = l;
     }
