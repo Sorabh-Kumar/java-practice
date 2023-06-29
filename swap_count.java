bad0207c71e82873edf2f4d58269d907412c67e9
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-06-29 23:11:41 +0530

    intial commit

---
+++ swap_count.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class swap_count {
+    public static void sort(int[] arr){
+        int l = arr.length;
+        int[] a = new int[l];
+        for(int i = 1; i<l; i++){
+            int p = arr[i];
+            for(int j = i; j<=0 && arr[j]< p; j--){
+                 System.out.println("sort");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+        for(int i =0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+        sort(a);
+    }
+}
