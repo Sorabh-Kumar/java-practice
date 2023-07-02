505077d4c684d2be0ef916212dd9bcdb0b5f2ead
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-07-02 22:47:17 +0530

    intial commit

--- target_sum_triplet.java
+++ target_sum_triplet.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
-public class target_sum {
+public class target_sum_triplet {
     public static void target(int[] a, int n){
-        int l = 0;
-        int r = a.length -1;
-        while(l<r){
-            int sum = a[l] + a[r];
+        int x = a.length;
+        
+        for(int i =0; i<x;i++ ){
+            int l = i;
+            int c = i+1;
+            int r = x -1;
+        while(c<r){
+            int sum = a[c]+a[l] + a[r];
             if(sum > n){
                 r--;
             }
             else if(sum < n){
-                l++;
+                c++;
             }
             else{
-                System.out.println(a[l]+" and "+a[r]);
-                l++;
+                System.out.println(a[l]+", "+a[c]+" and "+a[r]);
                 r--;
+                c++;
             }
-        }
+        }}
     }
     public static void sort(int[] arr){
         int l = arr.length;
