0250fa3bb550371a3cac9246f223478a232ac469
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-07-04 23:55:20 +0530

    initial commit

---
+++ array2d.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class array2d {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] a = new int[3][5];
+    }
+}
