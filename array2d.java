d644be99e8d7133c69d360857f8eea7481dad92b
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-07-06 23:54:08 +0530

    changes

--- array2d.java
+++ array2d.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class array2d {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] a = new int[3][5];
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] a = new int[m][n];
+        for(int i = 0; i<n; i++){
+            for(int j =0 ; j<m; j++){
+                a[j][i] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i<n; i++){
+            for(int j =0 ; j<m; j++){
+                System.out.print(a[j][i]);
+            }
+            System.out.println();
+        }
     }
 }
