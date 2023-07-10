d5ada7696aa76f2e35f0e8cc13e728af2168453b
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-07-10 23:44:32 +0530

    changes

--- rotate_anti_2d_array.java
+++ rotate_anti_2d_array.java
@@ -23,6 +23,12 @@ public class rotate_anti_2d_array {
                 }
             }
         }
+        for(int i = 0; i<m; i++){
+            for(int j =0 ; j<m; j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
