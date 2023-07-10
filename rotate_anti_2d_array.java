f3608e32dc8315ebfbf236b8409914c88e9ab2f1
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-07-10 23:42:42 +0530

    changes

--- rotate_anti_2d_array.java
+++ rotate_anti_2d_array.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class rotate_anti_2d_array {
     public static void rotate(int[][] a) {
-        
+        int m =a.length;
+        for(int i=0; i<m; i++){
+            int s=0;
+            int e=m-1;
+            while(s<e){
+                int temp = a[i][s];
+                a[i][s] = a[i][e];
+                a[i][e] = temp;
+                s++;
+                e--;
+            }
+        }
     }
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
