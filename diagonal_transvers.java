cc0618fc24c6ee6fec91ab3be72d02b4a79d43b2
Sorabh <Sorabh.kumar.4a@gmail.com>
2023-07-13 22:45:15 +0530

    completion

--- diagonal_transvers.java
+++ diagonal_transvers.java
@@ -3,7 +3,43 @@ public class diagonal_transvers {
     public static void diagonal(int[][] a) {
         int m = a.length;
         int n = a[0].length;
-        for(int i =0; i<m; i++)
+        int r = 0;
+        int c= 0;
+        int i = 0; 
+        int [] x = new int[m*n];
+        boolean up = true;
+        while(r<m && c<n){
+            if(up){
+                while(r>0&& c<n-1){
+                x[i++] = a[r][c];
+                r--;
+                c++;}
+                x[i++] = a[r][c];
+                if(c==n-1){
+                    r++;
+                }
+                else{
+                    c++;
+                }
+            }
+            else{
+                while(r<m-1 && c>0){
+                x[i++] = a[r][c];
+                r++;
+                c--;}
+                x[i++] = a[r][c];
+                if(r==m-1){
+                    c++;
+                }
+                else{
+                    r++;
+                }
+            }
+            up = !up;
+        }
+        for(int z=0; z<(m*n); z++){
+			System.out.print(x[z]+" ");
+		}
     }
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
